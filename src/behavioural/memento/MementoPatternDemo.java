cdcc9b3cf64eed8f00d4edf6256d30083e9cf10e
VenkateshSubramaniyan <srvenkat16@gmail.com>
2024-03-17 17:28:21 +0530

    Memo/ Memento pattern Documented

--- src/behavioural/memento/MementoPatternDemo.java
+++ src/behavioural/memento/MementoPatternDemo.java
@@ -1,6 +1,16 @@
 package behavioural.memento;
 
-
+/**
+ * The memo pattern provides flexibility to restore an object to its previous state (undo) (Previous checkpoint)
+ * or store the object's history to understand how it mutated over several operations and restore to a specific state when required.
+ *
+ * Originator: This object is responsible for creating a memo that can store and restore its internal state
+ *                  and decide which internal state memento object stores according to required needs.
+ * Memento:    This object is used to store the internal state/Snapshot of the Originator
+ *                  and can prevent objects (other than Originator) from accessing this object.
+ * Caretaker: This object store snapshot  or return the requested snapshot
+ *
+ */
 public class MementoPatternDemo {
     public static void main(String[] args) {
 
